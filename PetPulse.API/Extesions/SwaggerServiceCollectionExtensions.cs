4e8380c2f4731d35983d1f8c50c95e9819f2f46d
PietroWilhelm <pietrowilhelm@git.com>
2026-05-05 16:34:02 -0300

    feat: adicionar configuração do Swagger e strings de conexão ao PetPulse API

---
+++ PetPulse.API/Extesions/SwaggerServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using Microsoft.OpenApi;
+
+namespace PetPulse.Extesions;
+
+/// <summary>
+/// Configuração do Swagger/OpenAPI com interface Swagger UI via Swashbuckle.
+/// </summary>
+public static class SwaggerServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adiciona geração de documento OpenAPI e metadados para o Swagger UI.
+    /// </summary>
+    /// <param name="services">Coleção de serviços.</param>
+    /// <param name="configuration">Configuração da aplicação.</param>
+    /// <returns>A mesma instância para encadeamento.</returns>
+    public static IServiceCollection addPetPulseSwagger(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = configuration.GetSection("Swagger:Title").Value,
+                Version = "v1",
+                Description = "API para acompanhamento preventivo da saúde de pets, cadastro de tutores, histórico clínico, dispositivos IoT e alertas inteligentes."
+            });
+
+            var xml = Path.Combine(
+                AppContext.BaseDirectory,
+                $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"
+            );
+
+            if (File.Exists(xml))
+                options.IncludeXmlComments(xml, includeControllerXmlComments: true);
+        });
+
+        return services;
+    }
+}
--- PetPulse.API/Program.cs
+++ PetPulse.API/Program.cs
@@ -1,17 +1,28 @@
+using PetPulse.Extesions;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddPetPulseDbContext(builder.Configuration);
+
+builder.Services.AddPetPulseRepositories();
+
+builder.Services.addPetPulseSwagger(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "PetPulse API v1");
+    });
 }
 
 app.UseHttpsRedirection();
